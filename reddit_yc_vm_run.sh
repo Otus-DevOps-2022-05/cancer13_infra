4257654eee974fd4ba6a8ee18f1eeabf7612b189
Ivan Kuznetsov <cancer13@protonmail.com>
2022-06-30 22:38:13 +0300

    fix install_ruby.sh filename

--- reddit_yc_vm_run.sh
+++ reddit_yc_vm_run.sh
@@ -7,3 +7,7 @@ yc compute instance create \
   --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
   --metadata serial-port-enable=1 \
   --ssh-key ~/.ssh/appuser.pub
+
+sleep 60 # wait vm is up
+
+ssh -i ~/.ssh/appuser appuser@$(yc compute instance get reddit-app --format json | jq -r '.network_interfaces[].primary_v4_address.one_to_one_nat.address') "bash -s" < ./install_ruby.sh ./install_mongodb.sh ./deploy.sh
